cc203e02795570452a96839c0ccb9df1bb8198bc
kolena <kolena@avast.com>
2019-05-18 00:36:55 +0200

    Transmitter: - extract some constants - configure identity

--- transmitter/maze-unlock-transmitter.ino
+++ transmitter/maze-unlock-transmitter.ino
@@ -2,38 +2,40 @@
 #include <SPI.h>
 #include <RF24.h>
 
-//create an RF24 object
-RF24 radio(9, 8);  // CE, CSN
+#define MU_PIN_CE 9
+#define MU_PIN_CSN 8
+#define MU_PIN_IDENTITY 7
+#define MU_PIN_JOY A6
 
-//address through which two modules communicate.
-const byte address[6] = "00001";
+RF24 radio(MU_PIN_CE, MU_PIN_CSN);
+
+const byte address[6] = "1tran";
+const byte address2[6] = "2tran";
 
 void setup() {
     Serial.begin(9600);
+    delay(100);
 
     pinMode(6, OUTPUT);
-    pinMode(7, INPUT_PULLUP);
+    pinMode(5, OUTPUT);
+    pinMode(MU_PIN_IDENTITY, INPUT_PULLUP);
 
-//    int val = 0 +  (((digitalRead(7) + 1) % 2) << 0);
-//    Serial.println(val, BIN);
-//    itoa(val, address);
+    int val = 0 + (((digitalRead(MU_PIN_IDENTITY) + 1) % 2) << 0); // NOLINT(hicpp-signed-bitwise)
 
-    radio.begin();
+    if (!radio.begin()) Serial.println("Could not initialize radio!!!"); else Serial.println("Radio initialized");
 
-    radio.openWritingPipe(address);
+    radio.openWritingPipe(val == HIGH ? address : address2);
     radio.stopListening();
 }
 
 void loop() {
-    int value = map((int) fabs(analogRead(A6)), 0, 1023, 0, 255);
-    Serial.print("Level: ");
-    Serial.println(value);
+    int value = map((int) fabs(analogRead(MU_PIN_JOY)), 0, 1023, 0, 255);
 
     if (digitalRead(3) == LOW) {
         digitalWrite(6, HIGH);
     } else digitalWrite(6, LOW);
 
-    if (digitalRead(7) == LOW) {
+    if (digitalRead(MU_PIN_IDENTITY) == LOW) {
         digitalWrite(5, HIGH);
     } else {
         digitalWrite(5, LOW);
@@ -42,4 +44,6 @@ void loop() {
     char text[4];
     itoa(value, text, 10);
     radio.write(&text, sizeof(text));
+
+    delay(15);
 }
