31b7efe306898d65c4f84b633e3e1a1b2a789c55
kolena <kolena@avast.com>
2019-05-19 02:39:16 +0200

    Sending progress from clients. Configuring channel number through dip switches. Transmitter pings receiver every second and displays green light if succeeds. Schemas update.

--- receiver/maze-unlock-receiver.ino
+++ receiver/maze-unlock-receiver.ino
@@ -2,49 +2,240 @@
 #include <SPI.h>
 #include <RF24.h>
 #include <Adafruit_NeoPixel.h>
+#include <printf.h>
 
+#define MU_CHANNEL_OFFSET 105
 #define MU_PIN_CE 9
 #define MU_PIN_CSN 8
-#define MU_PIN_LED 8
+#define MU_PIN_LED 5
+#define MU_PIN_BUZZER 6
+#define MU_PINS_CHANNEL {A0, A1, A2}
+#define MU_LEDS 24
+#define MU_LED_ANIM_DELAY 40
 
 RF24 radio(MU_PIN_CE, MU_PIN_CSN);
 
 const byte address[6] = "1tran";
 const byte address2[6] = "2tran";
 
-Adafruit_NeoPixel LED = Adafruit_NeoPixel(24, MU_PIN_LED, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel LED = Adafruit_NeoPixel(MU_LEDS, MU_PIN_LED, NEO_GRB + NEO_KHZ800);
 
-int blue = 0, red = 0;
+int scores[2] = {0, 0};
+
+void beep(int length) {
+    digitalWrite(MU_PIN_BUZZER, HIGH);
+    delay(length);
+    digitalWrite(MU_PIN_BUZZER, LOW);
+    delay(10);
+}
+
+int digitalReadPullup(int pin) {
+    return (digitalRead(pin) + 1) % 2;
+}
+
+int getChannelNumber(const int *channelPins) {
+//    int channel = 0;
+//    if (digitalReadPullup(channelPins[0])) channel = bitSet(channel, 1);
+//    if (digitalReadPullup(channelPins[1])) channel = bitSet(channel, 2);
+//    if (digitalReadPullup(channelPins[2])) channel = bitSet(channel, 3);
+//    return MU_CHANNEL_OFFSET + channel;
+
+    // TODO fix after wiring
+    return 109;
+}
+
+void initRadio() {
+    if (!radio.begin()) {
+        Serial.println("Could not initialize radio!!!");
+        for (int i = 0; i < 5; i++) {
+            beep(1000);
+            delay(200);
+        }
+    } else Serial.println("Radio initialized");
+}
 
 void setup() {
     Serial.begin(9600);
+    Serial.println("\n---\nStarting...\n");
+    printf_begin();
     delay(100);
 
-    if (!radio.begin()) Serial.println("Could not initialize radio!!!"); else Serial.println("Radio initialized");
+    int channelPins[] = MU_PINS_CHANNEL;
+
+    pinMode(channelPins[0], INPUT_PULLUP);
+    pinMode(channelPins[1], INPUT_PULLUP);
+    pinMode(channelPins[2], INPUT_PULLUP);
+
+    initRadio();
+
+    radio.setAutoAck(true);
+    radio.enableAckPayload();
+    radio.enableDynamicPayloads();
+    radio.setDataRate(RF24_250KBPS);
+    radio.setRetries(10, 15);
+    radio.setPALevel(RF24_PA_MAX);
+
+    int channel = getChannelNumber(channelPins);
+    radio.setChannel(channel);
 
     radio.openReadingPipe(1, address);
     radio.openReadingPipe(2, address2);
     radio.startListening();
     LED.begin();
+    radio.printDetails();
+    Serial.println();
+
+    LED.setBrightness(20);
+    LED.show();
+
+    for (int i = 0; i < MU_LEDS; i++) {
+        delay(MU_LED_ANIM_DELAY);
+        LED.setPixelColor(i, 255, 255, 255);
+        LED.show();
+    }
+
+    delay(MU_LED_ANIM_DELAY * 2);
+
+    for (int i = 0; i < MU_LEDS; i++) {
+        LED.setPixelColor(i, 0, 0, 0);
+    }
+    LED.show();
 }
+
+int clientProgress(int clientId) {
+    scores[clientId - 1]++;
+
+    int common = min(MU_LEDS, min(scores[0], scores[1]));
+
+    for (int i = 0; i < common; i++) {
+        LED.setPixelColor(i, 0, 255, 255);
+    }
+
+    for (int i = common; i < scores[0]; i++) {
+        LED.setPixelColor(i, 0, 255, 0);
+    }
+
+    for (int i = common; i < scores[1]; i++) {
+        LED.setPixelColor(i, 0, 0, 255);
+    }
+
+    LED.show();
+    beep(500);
+
+    return scores[clientId - 1];
+}
+
+
+void finish(int clientId) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+    while (true) {
+        // on:
+        for (int i = 0; i < MU_LEDS; i++) {
+            switch (clientId) {
+                case 1:
+                    LED.setPixelColor(i, 0, 255, 0);
+                    break;
+                case 2:
+                    LED.setPixelColor(i, 0, 0, 255);
+                    break;
+            }
+        }
+        LED.show();
+        digitalWrite(MU_PIN_BUZZER, HIGH);
+        delay(500);
+
+        // off:
+        for (int i = 0; i < MU_LEDS; i++) {
+            LED.setPixelColor(i, 0, 0, 0);
+        }
+        LED.show();
+        digitalWrite(MU_PIN_BUZZER, LOW);
+        delay(500);
+    }
+#pragma clang diagnostic pop
+}
+
+//void updateLevel(int clientId, char *data) {
+//    int level = atoi(data);
+//
+////        Serial.print("Sender: ");
+////        Serial.println(clientId);
+//
+//    Serial.print("level: ");
+//    Serial.println(level);
+//
+//    if (clientId == 1) red = level; else blue = level;
+//
+//    for (int i = 0; i < MU_LEDS; i++) {
+//        LED.setPixelColor(i, red, blue, 10);
+//    }
+//
+////    switch (clientId) {
+////        case 1:
+////            client1++;
+////            break;
+////        case 2:
+////            client2++;
+////            break;
+////    }
+////
+////
+////    int common = min(MU_LEDS, min(client1, client2));
+////
+////    for (int i = 0; i < common; i++) {
+////        LED.setPixelColor(i, 0, 255, 255);
+////    }
+////
+////    for (int i = common; i < client1; i++) {
+////        LED.setPixelColor(i, 0, 255, 0);
+////    }
+////
+////    for (int i = common; i < client2; i++) {
+////        LED.setPixelColor(i, 0, 0, 255);
+////    }
+//
+//    LED.show();
+//}
 
 void loop() {
     byte clientId = 0;
 
     if (radio.available(&clientId)) {
-        char text[3] = {0};
-        radio.read(&text, sizeof(text));
-        int level = atoi(text);
+        char rawData[10] = {0};
+        radio.read(&rawData, sizeof(rawData));
 
-//        Serial.print("Sender: ");
-//        Serial.println(clientId);
+//        Serial.println(rawData);
 
-        if (clientId==1) red = level; else blue = level;
+        char *data = *(&rawData) + 1;
 
-        for (int i = 0; i < 24; i++) {
-            LED.setPixelColor(i, red, blue, 10);
+        switch (rawData[0]) {
+            case 'i': {
+                Serial.println("PING");
+            }
+                break;
+//            case 'u': {
+//                updateLevel(clientId, data);
+//            }
+//                break;
+
+            case 'p': {
+                int clientScore = clientProgress(clientId);
+
+                if (clientScore >= MU_LEDS) finish(clientId);
+
+//                char ackPayload[3];
+//                itoa(clientScore, ackPayload, 10);
+//                Serial.print("Sending back: ");
+//                Serial.println(ackPayload);
+//                radio.writeAckPayload(clientId, ackPayload, sizeof(ackPayload));
+            }
+                break;
+
+            default: {
+                Serial.print("Not a valid command: ");
+                Serial.println(rawData);
+            }
         }
-
-        LED.show();
     }
 }
--- transmitter/maze-unlock-transmitter.ino
+++ transmitter/maze-unlock-transmitter.ino
@@ -1,49 +1,155 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <RF24.h>
+#include "printf.h"
 
+#define MU_CHANNEL_OFFSET 105
 #define MU_PIN_CE 9
 #define MU_PIN_CSN 8
 #define MU_PIN_IDENTITY 7
-#define MU_PIN_JOY A6
+#define MU_PIN_JOY_X A6
+#define MU_PIN_BUZZER 6
+#define MU_PIN_LED_RED 5
+#define MU_PIN_LED_GREEN 4
+#define MU_PINS_CHANNEL {A0, A1, A2}
 
 RF24 radio(MU_PIN_CE, MU_PIN_CSN);
 
 const byte address[6] = "1tran";
 const byte address2[6] = "2tran";
 
+void blinkRed(int length) {
+    digitalWrite(MU_PIN_LED_RED, HIGH);
+    delay(length);
+    digitalWrite(MU_PIN_LED_RED, LOW);
+
+    delay(10);
+}
+
+void blinkGreen(int length) {
+    digitalWrite(MU_PIN_LED_GREEN, HIGH);
+    delay(length);
+    digitalWrite(MU_PIN_LED_GREEN, LOW);
+
+    delay(10);
+}
+
+void beep(int length) {
+//    digitalWrite(MU_PIN_BUZZER, HIGH);
+//    delay(length);
+//    digitalWrite(MU_PIN_BUZZER, LOW);
+//    delay(10);
+}
+
+int digitalReadPullup(int pin) {
+    return (digitalRead(pin) + 1) % 2;
+}
+
+int getChannelNumber(const int *channelPins) {
+    int channel = 0;
+    if (digitalReadPullup(channelPins[0])) channel = bitSet(channel, 1);
+    if (digitalReadPullup(channelPins[1])) channel = bitSet(channel, 2);
+    if (digitalReadPullup(channelPins[2])) channel = bitSet(channel, 3);
+    return MU_CHANNEL_OFFSET + channel;
+}
+
+void initRadio() {
+    if (!radio.begin()) {
+        Serial.println("Could not initialize radio!!!");
+        for (int i = 0; i < 5; i++) {
+            beep(1000);
+            delay(200);
+        }
+    } else Serial.println("Radio initialized");
+}
+
 void setup() {
     Serial.begin(9600);
+    Serial.println("\n---\nStarting...\n");
+    printf_begin();
     delay(100);
 
-    pinMode(6, OUTPUT);
-    pinMode(5, OUTPUT);
+    int channelPins[] = MU_PINS_CHANNEL;
+
+    pinMode(channelPins[0], INPUT_PULLUP);
+    pinMode(channelPins[1], INPUT_PULLUP);
+    pinMode(channelPins[2], INPUT_PULLUP);
+
+    pinMode(MU_PIN_BUZZER, OUTPUT);
+    pinMode(MU_PIN_LED_RED, OUTPUT);
+    pinMode(MU_PIN_LED_GREEN, OUTPUT);
     pinMode(MU_PIN_IDENTITY, INPUT_PULLUP);
 
-    int val = 0 + (((digitalRead(MU_PIN_IDENTITY) + 1) % 2) << 0); // NOLINT(hicpp-signed-bitwise)
+    int identitySwitch = digitalReadPullup(MU_PIN_IDENTITY) << 0;
 
-    if (!radio.begin()) Serial.println("Could not initialize radio!!!"); else Serial.println("Radio initialized");
+    Serial.println(identitySwitch == HIGH ? "ClientId: 2" : "ClientId: 1");
 
-    radio.openWritingPipe(val == HIGH ? address : address2);
+    initRadio();
+
+    radio.setAutoAck(true);
+    radio.enableAckPayload();
+    radio.enableDynamicPayloads();
+    radio.setDataRate(RF24_250KBPS);
+    radio.setRetries(10, 15);
+    radio.setPALevel(RF24_PA_MAX);
+
+    int channel = getChannelNumber(channelPins);
+    radio.setChannel(channel);
+
+    radio.openWritingPipe(identitySwitch == HIGH ? address : address2);
     radio.stopListening();
+
+    radio.printDetails();
+    Serial.println();
 }
 
 void loop() {
-    int value = map((int) fabs(analogRead(MU_PIN_JOY)), 0, 1023, 0, 255);
+//    if (digitalRead(3) == LOW) {
+//        digitalWrite(6, HIGH);
+//    } else digitalWrite(6, LOW);
 
-    if (digitalRead(3) == LOW) {
-        digitalWrite(6, HIGH);
-    } else digitalWrite(6, LOW);
+    int value = map((int) fabs(analogRead(MU_PIN_JOY_X)), 0, 1023, 0, 255);
 
-    if (digitalRead(MU_PIN_IDENTITY) == LOW) {
-        digitalWrite(5, HIGH);
-    } else {
-        digitalWrite(5, LOW);
+    if (value > 200) {
+        char rawData[2] = "p";
+
+        if (radio.write(&rawData, sizeof(rawData))) {
+            Serial.print("Sent: ");
+            Serial.println(rawData);
+
+//            if (radio.isAckPayloadAvailable()) {
+//                char ackPayload[3];
+//                radio.read(&ackPayload, sizeof(ackPayload));
+//                Serial.print("ACK payload: ");
+//                Serial.println(ackPayload);
+//            }
+
+            beep(15);
+            delay(400);
+        } else {
+            Serial.println("Could not sent data!");
+            beep(100);
+            blinkRed(50);
+//            beep(100);
+//            beep(300);
+        }
     }
 
-    char text[4];
-    itoa(value, text, 10);
-    radio.write(&text, sizeof(text));
+    if ((millis() / 100) % 10 == 0) {
+        char rawData[2] = "i";
+        if (radio.write(&rawData, sizeof(rawData))) {
+            blinkGreen(30);
+        } else {
+            Serial.println("Could not sent ping!");
+            beep(100);
+            blinkRed(50);
+            beep(100);
+            blinkRed(50);
+            beep(300);
+            blinkRed(50);
+        }
 
-    delay(15);
+        delay(100);
+    }
+
 }
